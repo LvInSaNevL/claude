d41be6ac8416a1020e704315c7fafd09b04077a8
Lv_InSaNe_vL <mattwollam@gmail.com>
2021-11-04 20:22:10 -0400

    basic operation
    Claude now figures out which games are installed on your PC and can run them. Steam only, and the buttons just have the game ID lol. But the idea works.

---
+++ Claude/Claude/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Claude
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> steamGames = Steam.Installed();
+            
+            foreach (string t in steamGames)
+            {
+                Button gameButton = new Button { Content = t };
+                gameButton.Click += GameButtonClick;
+                gameButton.Tag = "steam";
+                windowMain.Children.Add(gameButton);
+            };
+        }
+
+        public static void GameButtonClick(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            Process process = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C start steam://rungameid/" + button.Content;
+
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+        
+    }
+}
---
+++ Claude/Claude/Steam.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Gameloop.Vdf;
+using Gameloop.Vdf.JsonConverter;
+using System;
+using System.Collections.Generic;
+using Gameloop.Vdf.Linq;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+
+public class Steam
+{
+    private static readonly string baseLocation = "C:\\Program Files (x86)\\Steam";
+	public static List<string> Installed()
+    {
+        string installList = baseLocation + "\\steamapps\\libraryfolders.vdf";
+        VProperty rawFolders = VdfConvert.Deserialize(File.ReadAllText(installList));
+        JToken libraryFolders = rawFolders.ToJson().Last;
+
+        List<string> appList = new List<string>();
+        int counter = 0;
+        for (int s = 0; s < (libraryFolders.Count() - 1); s++)
+        {
+            try
+            {
+                var temp = libraryFolders[s.ToString()]["apps"];
+                foreach (var g in temp)
+                {
+                    string[] bits = g.ToString().Split(":");
+                    appList.Add(bits[0].Replace("\"", ""));
+                }
+
+                counter++;
+            }
+            catch (Exception) { Console.WriteLine("Null reference exception"); }
+        };
+
+        return appList;
+        
+    }
+
+    
+}
